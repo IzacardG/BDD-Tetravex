915bfa41b3e17894b38a6429bebc35df91f7ac23
fandejuni <t.dardinier@gmail.com>
2017-05-14 21:58:11 +0200

    Added tests

--- test.ml
+++ test.ml
@@ -1,12 +1,5 @@
-(*
 let formule = Or(Imp(Var("p"), Var("q")), And(Var("r"), Var("s"))) in
-let big_tree = buildTree formule in
-printBdd(reduceTreeTobdd big_tree);
-*)
-
-let formule = Or(Imp(Var("p"), Var("q")), And(Var("r"), Var("s"))) in
-let reduced_tree = reduceTree (buildTree formule) in
-let bdd = reduceTreeTobdd reduced_tree in
+let bdd = createBdd formule in
 let valuation = emptyValuation() in
 assert(evaluateBdd valuation bdd);
 setValue valuation "p" true;
@@ -18,7 +11,6 @@ setValue valuation "r" true;
 assert(not (evaluateBdd valuation bdd));
 setValue valuation "s" true;
 assert(evaluateBdd valuation bdd);
-(* printBdd(bdd); *)
 
 let formule = And(Var("x"), Not(Var("x"))) in
 assert(estValide (noBdd (createBdd formule)));
@@ -26,8 +18,10 @@ assert(estValide (noBdd (createBdd formule)));
 let formule = Imp(Var("a"), Var("a")) in
 assert(estValide (createBdd formule));
 
-(*
-let big_tree = buildTree formule;
-let big_bdd = reduceTreeTobdd big_tree;
-let reduced_bdd = reduceTreeTobdd reduced_tree;
-*)
+print_string "Petit BDD\n";
+let formule = Or(Imp(Var("p"), Var("q")), And(Var("r"), Var("s"))) in
+printBdd(createBdd formule);
+
+print_string "\nGros BDD\n";
+let big_tree = buildTree formule in
+printBdd (reduceTreeTobdd big_tree);
